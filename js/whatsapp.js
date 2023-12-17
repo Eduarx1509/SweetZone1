b4e31f8af7c020e7025280595613f5bd0c1ccd45
Eduarx1509 <eduarx150903@gmail.com>
2023-12-16 23:18:38 -0500

    cambiodenumero

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,5 +1,5 @@
 function enviarMensaje(producto) {
-    var numeroTelefono = '51910645892';
+    var numeroTelefono = '51970728078';
     var mensaje = encodeURIComponent('¡Hola! Quisiera solicitar información sobre ' + producto);
     var url = 'https://api.whatsapp.com/send/?phone=' + numeroTelefono + '&text=' + mensaje + '&type=phone_number&app_absent=0';
     window.open(url, '_blank');
